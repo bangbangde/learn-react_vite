cecbb44d71c2e8716fd503ab61f1db86713def0b
frank <chengqifw@gmail.com>
2022-08-29 18:29:15 +0800

    feat: update

---
+++ src/advanced-guides/21-uncontrolled-components/main.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+class NameForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.input = React.createRef();
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.input.current.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input type="text" ref={this.input} defaultValue="Bob" />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
+class FileInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.fileInput = React.createRef();
+  }
+  handleSubmit(event) {
+    event.preventDefault();
+    alert(
+      `Selected file - ${this.fileInput.current.files[0].name}`
+    );
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Upload file:
+          <input type="file" ref={this.fileInput} />
+        </label>
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+    );
+  }
+}
+
+
+const element = (
+  <div>
+    <h1>21-uncontrolled-components</h1>
+    <NameForm /> 
+    <FileInput />
+  </div>
+);
+
+ReactDOM.createRoot(document.getElementById('root')).render(element);
